51cbf89ea5979c38daf906e2a852bc46e5c9ecf0
guruK7112 <guruprasad0733@gmail.com>
2025-03-05 10:16:35 +0530

    ADDED NEW INFO TO CREATE ONTACT TEST

--- TollOrProjectName/src/test/java/pac2/orgTest.java
+++ TollOrProjectName/src/test/java/pac2/orgTest.java
@@ -1,19 +1,29 @@
 package pac2;
 
-import java.lang.reflect.Method;
 
+import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
 
 public class orgTest  {
 	
 	@Test
-public void createOrgTest(Method mth) {
-	System.out.println("execute " +mth.getName());
+public void createOrgTest() {
+		System.out.println("execte createOrgTest")	;
+	String  URL=System.getProperty("url","http://localhost:8888");  //default if url not work
+	String BROWSER=System.getProperty("browser","chrome");          //default if browser not work
+	String USERNAME=System.getProperty("username","admin");         //default if username not work
+	String PASSWORD=System.getProperty("password","password");      //default if password not work
+	
+	System.out.println(URL);
+	System.out.println(BROWSER);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
+	
 }
 	
 	@Test
-	public void modifyOrgtTest(Method mth) {
-	System.out.println("execute "+mth.getName());
+	public void modifyOrgtTest() {
+	System.out.println("execute modifyOrgTest");
 }
 }
