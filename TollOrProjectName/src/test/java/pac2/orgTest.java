9d42189d83319754293e4ab81e54475bcd68b018
guruK7112 <guruprasad0733@gmail.com>
2025-03-05 12:09:13 +0530

    same piece of code

--- TollOrProjectName/src/test/java/pac2/orgTest.java
+++ TollOrProjectName/src/test/java/pac2/orgTest.java
@@ -24,5 +24,14 @@ public void createOrgTest() {
 	@Test
 	public void modifyOrgtTest() {
 	System.out.println("execute modifyOrgTest");
+	String  URL=System.getProperty("url","http://localhost:8888");  //default if url not work
+	String BROWSER=System.getProperty("browser","chrome");          //default if browser not work
+	String USERNAME=System.getProperty("username","admin");         //default if username not work
+	String PASSWORD=System.getProperty("password","password");      //default if password not work
+	
+	System.out.println(URL);
+	System.out.println(BROWSER);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
 }
 }
